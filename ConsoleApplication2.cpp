bc84006f908001a8f1b532d29f6098d26880c808
nnaammee <51121246+nnaammee@users.noreply.github.com>
2021-12-19 17:57:01 +0300

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,176 @@
+﻿//#include <iostream>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/opencv.hpp>
+//#include <vector>
+//
+//using namespace std;
+//using namespace cv;
+//
+//
+//Mat Mask(Mat image, vector<vector<double>> filter) {
+//		
+//	Mat result;
+//	image.copyTo(result);
+//	
+//	
+//	for (int i = (int)filter[0].size() / 2; i < image.rows - (int)filter[0].size() / 2; i++)
+//	    for (int j = (int)filter[0].size() / 2 ; j < image.cols - (int)filter[0].size() / 2; j++)
+//	    {
+//	        double cr = 0.0, cg = 0.0, cb = 0.0;
+//	
+//	        for (int wid = 0; wid < (int)filter.size(); wid++)
+//	            for (int heig = 0; heig < (int)filter[0].size(); heig++) {
+//	                Vec3b pixel = image.at<Vec3b>(i - (int)filter[0].size() / 2 + heig, j - (int)filter[0].size() / 2 + wid);
+//	                cr += pixel[2] * filter[wid][heig];
+//	                cg += pixel[1] * filter[wid][heig];
+//	                cb += pixel[0] * filter[wid][heig];
+//	            }
+//	
+//	        result.at<Vec3b>(i, j)[0] = cb;
+//	        result.at<Vec3b>(i, j)[1] = cg;
+//	        result.at<Vec3b>(i, j)[2] = cr;
+//	    }
+//	
+//	return result;
+//};
+//
+//int main() {
+//	Mat img = imread("C:/Users/olgak/OneDrive/ConsoleApplication2/x64/Debug/1.jpg");
+//	//Mat blur_img; 
+//	//img.copyTo(blur_img);
+//
+//	vector<vector<double>>mask_blur = { {0.04, 0.04, 0.04, 0.04, 0.04}, {0.04, 0.04, 0.04, 0.04, 0.04}, {0.04, 0.04, 0.04, 0.04, 0.04}, {0.04, 0.04, 0.04, 0.04, 0.04}, {0.04, 0.04, 0.04, 0.04, 0.04} };
+//	vector<vector<double>>mask_grad = { {0, -1, 0}, {0, 1, 0}, {0, 0,0} };
+//	
+//	Mat blur_img = Mask(img, mask_blur);
+//	
+//	imshow("", img);
+//	waitKey(0);
+//
+//
+//	imshow("", blur_img);
+//	waitKey(0); 
+//	Mat Gimg = imread("C:/Users/olgak/OneDrive/ConsoleApplication2/x64/Debug/1.jpg", IMREAD_GRAYSCALE);
+//	imshow("", Gimg);
+//	waitKey(0);
+//	Mat grad_img = Mask(Gimg, mask_grad);
+//	cout << "3" << endl;
+//
+//	
+//	
+//	
+//	imshow("", grad_img);
+//	waitKey(0);
+//
+//	cout << "1" << endl;
+//
+//
+//	return 0;
+//}
+//
+
+
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <string>
+
+
+using namespace cv;
+using namespace std;
+
+Mat ApplyFilter(Mat image, vector<vector<double>> filter)
+{
+    int filterRow = (int)filter.size();
+    int filterColumn = (int)filter[0].size();
+    int halfFilterSize = filterColumn / 2;
+
+
+    int height, width;
+
+    height = image.rows;
+    width = image.cols;
+
+    cout << "Width : " << width << endl;
+    cout << "Height: " << height << endl;
+
+    Mat result;
+    image.copyTo(result);
+
+
+    for (int i = halfFilterSize; i < height - halfFilterSize; i++)
+        for (int j = halfFilterSize; j < width - halfFilterSize; j++)
+        {
+            double cr = 0.0, cg = 0.0, cb = 0.0;
+
+            for (int wid = 0; wid < filterRow; wid++)
+                for (int heig = 0; heig < filterColumn; heig++) {
+                    Vec3b pixel = image.at<Vec3b>(i - halfFilterSize + heig, j - halfFilterSize + wid);
+                    cr += pixel[2] * filter[wid][heig];
+                    cg += pixel[1] * filter[wid][heig];
+                    cb += pixel[0] * filter[wid][heig];
+                }
+
+            result.at<Vec3b>(i, j)[0] = cb;
+            result.at<Vec3b>(i, j)[1] = cg;
+            result.at<Vec3b>(i, j)[2] = cr;
+        }
+
+    return result;
+};
+
+int main()
+{
+    // Лаба 2
+    Mat cat = imread("/Users/mythings/Documents/С++/repocpp/OpenCV.0/OpenCV.0/cobake.jpg");
+    imshow("showing something charming(1)", cat); //вывод оригинального изображения
+
+//    const vector<vector<double>> hardBlurMatrix = {
+//        {0.000789, 0.006581, 0.013347, 0.006581, 0.000789},
+//        {0.006581, 0.054901, 0.111345, 0.054901, 0.006581},
+//        {0.013347, 0.111345, 0.225821, 0.111345, 0.013347},
+//        {0.006581, 0.054901, 0.111345, 0.054901, 0.006581},
+//        {0.000789, 0.006581, 0.013347, 0.006581, 0.000789}
+//    };
+//
+//    const vector<vector<double>> simpleBlurMatrix = {
+//            { 0.1125, 0.1125, 0.1125},
+//            { 0.1125, 0.1, 0.1125},
+//            { 0.1125, 0.1125, 0.1125}
+//        };
+
+    const vector<vector<double>> mediumBlurMatrix = {
+        {0.04, 0.04, 0.04, 0.04, 0.04},
+        {0.04, 0.04, 0.04, 0.04, 0.04},
+        {0.04, 0.04, 0.04, 0.04, 0.04},
+        {0.04, 0.04, 0.04, 0.04, 0.04},
+        {0.04, 0.04, 0.04, 0.04, 0.04}
+    };
+
+    Mat myBlur = ApplyFilter(cat, mediumBlurMatrix);
+    imshow("Result 1:", myBlur); //вывод размытого изображения
+
+
+    //  Лаба 2.2
+
+    Mat im_gray = imread("/Users/mythings/Documents/С++/repocpp/OpenCV.0/OpenCV.0/cobake.jpg", IMREAD_GRAYSCALE);
+    imshow("showing something charming(2)", im_gray); //вывод оригинального изображения
+
+    const vector<vector<double>> gradNorthMatrix = {
+        { 0, -1, 0},
+        { 0, 1, 0},
+        { 0, 0, 0}
+    };
+
+
+    Mat grayMethod = ApplyFilter(im_gray, gradNorthMatrix);
+    imshow("Result 2:", grayMethod); //вывод градиент-изображения
+
+
+    waitKey(0);
+    return 0;
+
+}
